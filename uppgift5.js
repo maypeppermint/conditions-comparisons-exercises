648c3350ba1ce577d1c7e890985e80d8e9951349
Veronica Wu <maypeppermint@gmail.com>
2021-04-17 17:09:17 +0200

    5

--- uppgift5.js
+++ uppgift5.js
@@ -6,3 +6,18 @@ Kom ihåg att bryta ner problemet i flera moduler och få dessa att funka separa
 
 Parprogrammering uppmuntras. Använd er av tekniker från tidigare uppgifter för att lösa denna uppgift.
 */
+var move = ["rock", "scissors", "papper"];
+
+var random = Math.floor(Math.random() * move.length);
+console.log(random, move[random]);
+
+var yourMove = prompt("[0]Rock [1]Scissors  [2]Papper (Type a number from 0 1 2)");
+if (yourMove == random) {
+    yourMove = alert("Tie. Your opponent had " + move[random] + " too.");
+    
+}else if (yourMove - random == -1 || yourMove - random == 2) {
+    alert("You win! Your opponent had " + move[random] + ".");
+    
+} else {
+    alert("You lose! Your opponent had " + move[random] + ".");  
+}
